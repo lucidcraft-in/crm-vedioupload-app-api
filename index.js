0cfa56eb89f9336fbe85599b558bf3b700da80b7
Muhammed-Anees-P <aneespengad4447@gmail.com>
2025-05-30 17:11:55 +0530

    feat : cores enabled

--- index.js
+++ index.js
@@ -12,14 +12,13 @@ require('dotenv').config()
 
 connectDB()
  
-// app.use(cors({
-//     origin: "https://support-web.netlify.app",
-//     // origin:'http://localhost:5173',
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//     credentials:true
-// }))
-app.use(cors())
+app.use(cors({
+    origin: "https://support-web.netlify.app",
+    // origin:'http://localhost:5173',
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials:true
+}))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 
