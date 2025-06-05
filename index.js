8b158f836c3bcf0f31a5413a913519b103d61e31
lucidcraft-in <99498475+lucidcraft-in@users.noreply.github.com>
2025-06-05 20:58:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ require('dotenv').config()
 connectDB()
  
 app.use(cors({
-    origin: "https://support-web.netlify.app",
+    origin: '*',
     // origin:'http://localhost:5173',
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
