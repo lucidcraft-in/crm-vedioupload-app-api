7dc0fc28ab4b04d7b912df1bd5c67dae81272e76
lucidcraft-in <Salmanul.F@active-bytes.com>
2025-06-05 23:18:54 +0530

    routing

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -18,6 +18,14 @@ transporter = nodemailer.createTransport({
     }
 })
 
+router.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 //admin login
 router.post('/login', async(req,res) =>{
--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -30,7 +30,14 @@ const extractYouTubeVideoId = (url) => {
 };
   
 
-
+router.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  next();
+});
 
 //add videos
 router.post('/videos', upload.array('image'),async (req, res) =>{
