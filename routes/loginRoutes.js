5152f9d5b9b4daf9b71df4ff807cc0de91d7fe31
lucidcraft-in <Salmanul.F@active-bytes.com>
2025-06-05 23:36:16 +0530

    as

--- routes/loginRoutes.js
+++ routes/loginRoutes.js
@@ -18,14 +18,7 @@ transporter = nodemailer.createTransport({
     }
 })
 
-router.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
+ 
 
 //admin login
 router.post('/login', async(req,res) =>{
